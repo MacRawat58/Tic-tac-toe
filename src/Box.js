9be44ceae9a1b962ce394028c1923532ac43dc82
MacRawat58 <96304497+MacRawat58@users.noreply.github.com>
2023-04-12 10:49:34 +0530

    TiC-tac-toe app

---
+++ src/Box.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Icon from "./Icon";
+
+const Box = ()=>(
+    
+    <div className="box" >
+     <Icon /> 
+    </div>
+);
+
+export default Box;
